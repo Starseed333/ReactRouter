76c048451675f68be5ff37c354691929c3a13502
Sandra Marquez <sandra@Sandras-MacBook-Air.local>
2017-12-11 19:13:07 -0600

    mongolab

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGODB_URI || "mongodb://heroku_fplgvkk6:mqutg815c1fh5c1fgjuma4n9t0@ds137686.mlab.com:37686/heroku_fplgvkk6",
   {
     useMongoClient: true
   }
